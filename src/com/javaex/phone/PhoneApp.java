40500fdb2386cdbd2e0755fbe9abcbab54b49c0e
thunderkyg <thunderkyg@gmail.com>
2021-07-06 00:01:57 +0900

    OraclePro (2021/07/05)

--- src/com/javaex/phone/PhoneApp.java
+++ src/com/javaex/phone/PhoneApp.java
@@ -1,20 +1,12 @@
 package com.javaex.phone;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Writer;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class PhoneApp {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		// 스캐너
 		Scanner input = new Scanner(System.in);
